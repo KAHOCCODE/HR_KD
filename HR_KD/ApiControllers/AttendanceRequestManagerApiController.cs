c9e47169a0debaac9269514f18c03d937cb4c4fb
Pkmax-bit <2100010779@nttu.edu.vn>
2025-03-18 17:11:12 +0700

    Thêm yêu cầu chấm công từ quản lý tới nhân viên

---
+++ HR_KD/ApiControllers/AttendanceRequestManagerApiController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HR_KD.ApiControllers
+{
+    public class AttendanceRequestManagerApiController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}

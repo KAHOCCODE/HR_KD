b825f68809614d029c8533296f3d520ef26a160d
ka do doka <hoangkavudo@gmail.com>
2025-03-22 21:48:09 +0700

    -Tạo Model View Cho Đăng Nhập để  Xác thực dữ liệu ngay trên Model và RememberMe giúp nhớ đăng nhập - trang login ,accessDenied - sesion login

--- HR_KD/Program.cs
+++ HR_KD/Program.cs
@@ -1,21 +1,51 @@
-using HR_KD.Data;
+﻿using HR_KD.Data;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
+
+// Thêm hỗ trợ IHttpContextAccessor
+builder.Services.AddHttpContextAccessor();
+
+// Thêm Session vào hệ thống
+builder.Services.AddDistributedMemoryCache();
+builder.Services.AddSession(options =>
+{
+    options.IdleTimeout = TimeSpan.FromMinutes(30); // 30 phút hết hạn
+    options.Cookie.HttpOnly = true;
+    options.Cookie.IsEssential = true;
+});
+
 builder.Services.AddScoped<EmailService>();
 
-// Add services to the container.
-builder.Services.AddControllersWithViews();
+// Cấu hình Authentication 
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie(options =>
+    {
+        options.LoginPath = "/Account/Login";
+        options.AccessDeniedPath = "/Account/AccessDenied";
+    });
 
+builder.Services.AddAuthorization(options =>
+{
+    options.AddPolicy("EMPLOYEE", policy => policy.RequireRole("EMPLOYEE"));
+    options.AddPolicy("EMPLOYEE_MANAGER", policy => policy.RequireRole("EMPLOYEE_MANAGER"));
+    options.AddPolicy("LINE_MANAGER", policy => policy.RequireRole("LINE_MANAGER"));
+});
+
+// Cấu hình Entity Framework
 builder.Services.AddDbContext<HrDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("Db_HR")));
+
+// Thêm Controllers và Views
+builder.Services.AddControllersWithViews();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
@@ -24,6 +54,9 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+app.UseSession(); 
+
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllerRoute(

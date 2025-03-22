258375211399613564621bc72204c3ce3eecc4df
ka do doka <hoangkavudo@gmail.com>
2025-03-22 21:44:18 +0700

    sử dụng Authorize để Roles

--- HR_KD/Controllers/EmployeesController.cs
+++ HR_KD/Controllers/EmployeesController.cs
@@ -1,14 +1,16 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace HR_KD.Controllers
 {
     public class EmployeesController : Controller
     {
+        [Authorize(Roles = "EMPLOYEE")]
         public IActionResult Index()
         {
             return View();
         }
-
+        [Authorize(Roles = "EMPLOYEE_MANAGER")]
         public IActionResult Create()
         {
             return View();

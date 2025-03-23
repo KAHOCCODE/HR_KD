c6049162f962284e03901369c2415a72206ed807
ka do doka <hoangkavudo@gmail.com>
2025-03-23 23:18:08 +0700

    bỏ phân quyền role

--- HR_KD/Controllers/EmployeesController.cs
+++ HR_KD/Controllers/EmployeesController.cs
@@ -5,12 +5,12 @@ namespace HR_KD.Controllers
 {
     public class EmployeesController : Controller
     {
-        [Authorize(Roles = "EMPLOYEE")]
+        //[Authorize(Roles = "EMPLOYEE")]
         public IActionResult Index()
         {
             return View();
         }
-        [Authorize(Roles = "EMPLOYEE_MANAGER")]
+        //[Authorize(Roles = "EMPLOYEE_MANAGER")]
         public IActionResult Create()
         {
             return View();

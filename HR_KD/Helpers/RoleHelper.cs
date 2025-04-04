e3477379ce534ca84448c3f394e50800c03ad8b4
ka do doka <hoangkavudo@gmail.com>
2025-04-04 21:12:55 +0700

    thêm Helper để check nhiều role dễ hơn

---
+++ HR_KD/Helpers/RoleHelper.cs
@@ -0,0 +1,12 @@
+﻿using System.Security.Claims;
+
+namespace HR_KD.Helpers
+{
+    public static class RoleHelper
+    {
+        public static bool HasAnyRole(this ClaimsPrincipal user, params string[] roles)
+        {
+            return roles.Any(role => user.IsInRole(role));
+        }
+    }
+}

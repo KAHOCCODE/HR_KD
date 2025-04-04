9e42118579fadcbe90c280cfad3f1949b1bbf206
ka do doka <hoangkavudo@gmail.com>
2025-04-04 21:12:20 +0700

    thêm bảng LoginHistory để ghi lịch sử login của tk nv

---
+++ HR_KD/Data/LoginHistory.cs
@@ -0,0 +1,13 @@
+﻿namespace HR_KD.Data
+{
+    public class LoginHistory
+    {
+        public Guid LoginId { get; set; }
+        public string UserId { get; set; }
+        public string Username { get; set; }
+        public string Roles { get; set; }
+        public DateTime LoginTime { get; set; }
+        public string IPAddress { get; set; }
+        public string UserAgent { get; set; }
+    }
+}

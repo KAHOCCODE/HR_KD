175dc93dbfa20678f7f05d77e93258f2c7ae2fe3
ka do doka <hoangkavudo@gmail.com>
2025-04-12 23:11:07 +0700

    DTOs thêm role ,thêm file excel result, và thêm nhân viên

---
+++ HR_KD/DTOs/AssignRoleDto.cs
@@ -0,0 +1,8 @@
+﻿namespace HR_KD.DTOs
+{
+    public class AssignRoleDto
+    {
+        public string Username { get; set; } = null!;
+        public List<string> Roles { get; set; } = new();
+    }
+}
---
+++ HR_KD/DTOs/ExcelImportResult.cs
@@ -0,0 +1,8 @@
+﻿namespace HR_KD.DTOs
+{
+    public class ExcelImportResult
+    {
+        public List<ImportNhanVienDto> Employees { get; set; } = new();
+        public Dictionary<int, string> Errors { get; set; } = new();
+    }
+}

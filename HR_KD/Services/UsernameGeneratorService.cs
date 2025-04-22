5b9a9a67f3025fc49615c553b968c88c7c9edb7d
ka do doka <hoangkavudo@gmail.com>
2025-04-22 21:49:45 +0700

    Service Tạo tên nhanvien viết liền không dau + maNV

---
+++ HR_KD/Services/UsernameGeneratorService.cs
@@ -0,0 +1,39 @@
+﻿using System.Globalization;
+using System.Text;
+
+namespace HR_KD.Services
+{
+    public class UsernameGeneratorService
+    {
+        public string GenerateUsername(string fullName, int maNv)
+        {
+            if (string.IsNullOrWhiteSpace(fullName))
+                return "user" + maNv;
+
+            // Chuẩn hóa & loại bỏ dấu
+            string noDiacritics = RemoveDiacritics(fullName);
+
+            // Gộp lại tên không dấu + mã nhân viên
+            return noDiacritics.Replace(" ", "").ToLowerInvariant() + maNv;
+        }
+
+        private string RemoveDiacritics(string text)
+        {
+            var normalized = text.Normalize(NormalizationForm.FormD);
+            var builder = new StringBuilder();
+
+            foreach (var ch in normalized)
+            {
+                var unicodeCategory = CharUnicodeInfo.GetUnicodeCategory(ch);
+                if (unicodeCategory != UnicodeCategory.NonSpacingMark)
+                {
+                    builder.Append(ch);
+                }
+            }
+
+            return builder.ToString()
+                          .Replace("đ", "d")
+                          .Replace("Đ", "D");
+        }
+    }
+}

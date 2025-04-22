bf540acaa2ba03fcca6020dc0f1caa6edb3cbd2a
ka do doka <hoangkavudo@gmail.com>
2025-04-22 21:54:03 +0700

    service tạo randomkey

---
+++ HR_KD/Helpers/PasswordHelper.cs
@@ -0,0 +1,14 @@
+﻿namespace HR_KD.Helpers
+{
+    public class PasswordHelper
+    {
+        public static string GenerateRandomKey(int lenght = 16)
+        {
+            var randomkey = new Random();
+            var chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+            return new string(Enumerable.Repeat(chars, lenght)
+                .Select(s => s[randomkey.Next(s.Length)]).ToArray());
+
+        }
+    }
+}

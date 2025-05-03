c16f20cd8e4eca548e9a864da61296baa84e33e7
ka do doka <hoangkavudo@gmail.com>
2025-05-03 23:41:40 +0700

    RolePriorityHelper

---
+++ HR_KD/Helpers/RolePriorityHelper.cs
@@ -0,0 +1,27 @@
+﻿using System.Security.Claims;
+
+namespace HR_KD.Helpers
+{
+    public static class RolePriorityHelper
+    {
+        private static readonly List<string> RolePriority = new()
+        {
+            "DIRECTOR",
+            "LINE_MANAGER",
+            "EMPLOYEE_MANAGER",
+            "EMPLOYEE"
+        };
+
+        public static string GetHighestRole(this ClaimsPrincipal user)
+        {
+            return RolePriority.FirstOrDefault(r => user.IsInRole(r)) ?? "EMPLOYEE";
+        }
+
+        public static bool IsAtLeast(this ClaimsPrincipal user, string role)
+        {
+            int userLevel = RolePriority.IndexOf(GetHighestRole(user));
+            int requiredLevel = RolePriority.IndexOf(role);
+            return userLevel <= requiredLevel;
+        }
+    }
+}

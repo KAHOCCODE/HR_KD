ef978254c1b133d3bae5a84a29ddb792e84ee992
ka do doka <hoangkavudo@gmail.com>
2025-05-03 23:41:11 +0700

    PayrollDto

---
+++ HR_KD/DTOs/PayrollDto.cs
@@ -0,0 +1,19 @@
+﻿namespace HR_KD.DTOs
+{
+    internal class PayrollDto
+    {
+        public int MaLuong { get; set; }
+        public int MaNv { get; set; }
+        public DateOnly ThangNam { get; set; }
+        public decimal PhuCapThem { get; set; }
+        public decimal LuongThem { get; set; }
+        public decimal LuongTangCa { get; set; }
+        public decimal ThueTNCN { get; set; }
+        public decimal? TongLuong { get; set; }
+        public decimal ThucNhan { get; set; }
+        public string NguoiTao { get; set; }
+        public DateTime NgayTao { get; set; }
+        public string TrangThai { get; set; }
+        public string GhiChu { get; set; }
+    }
+}

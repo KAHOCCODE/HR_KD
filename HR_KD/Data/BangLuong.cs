1bed3f9557f84391bdfba7f03e005f2d0852e48a
ka do doka <hoangkavudo@gmail.com>
2025-05-03 23:40:48 +0700

    thêm cột ngày người duyệt vào bảng bangluong

--- HR_KD/Data/BangLuong.cs
+++ HR_KD/Data/BangLuong.cs
@@ -17,17 +17,15 @@ public partial class BangLuong
 
     public decimal LuongTangCa { get; set; } = 0;
 
-    // Remove BHXH, BHYT, and BHTN fields
-    // public decimal BHXH { get; set; } = 0;
-    // public decimal BHYT { get; set; } = 0;
-    // public decimal BHTN { get; set; } = 0;
-
     public decimal ThueTNCN { get; set; } = 0;
 
-    // Remove the computed column for TongLuong
     public decimal? TongLuong { get; set; }
 
     public decimal ThucNhan { get; set; }
+
+    public string? NguoiTao { get; set; }
+
+    public DateTime NgayTao { get; set; }
 
     public string? TrangThai { get; set; } = "Chưa thanh toán";
 

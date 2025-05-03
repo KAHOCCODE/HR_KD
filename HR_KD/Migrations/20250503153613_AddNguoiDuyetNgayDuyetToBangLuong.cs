d562a055f47c869ebfb3e1068140b99e8eb9d728
ka do doka <hoangkavudo@gmail.com>
2025-05-03 23:42:16 +0700

    thêm migration người,ngày duyệt vào bảng BangLuong

---
+++ HR_KD/Migrations/20250503153613_AddNguoiDuyetNgayDuyetToBangLuong.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HR_KD.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddNguoiDuyetNgayDuyetToBangLuong : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "NgayTao",
+                table: "BangLuong",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<string>(
+                name: "NguoiTao",
+                table: "BangLuong",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "NgayTao",
+                table: "BangLuong");
+
+            migrationBuilder.DropColumn(
+                name: "NguoiTao",
+                table: "BangLuong");
+        }
+    }
+}

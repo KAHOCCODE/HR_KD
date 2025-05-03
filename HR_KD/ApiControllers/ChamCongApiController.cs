e9e89ea5204e40d3c9250858ccb6d2fbe48681f7
ka do doka <hoangkavudo@gmail.com>
2025-05-03 23:14:50 +0700

    Chấm công APi

---
+++ HR_KD/ApiControllers/ChamCongApiController.cs
@@ -0,0 +1,30 @@
+﻿using HR_KD.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HR_KD.ApiControllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ChamCongApiController : ControllerBase
+    {
+        private readonly HrDbContext _context;
+
+        public ChamCongApiController(HrDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("GetByEmployeeAndMonth")]
+        public IActionResult GetChamCongByMonth(int maNv, int year, int month)
+        {
+            var chamCongs = _context.ChamCongs
+                .Where(c => c.MaNv == maNv &&
+                            c.NgayLamViec.Year == year &&
+                            c.NgayLamViec.Month == month)
+                .ToList();
+
+            return Ok(chamCongs);
+        }
+    }
+}
